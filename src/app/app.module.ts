916bb21a4f6fc98bb34f5a664ab4923159a961ab
Pratik1005 <pratikkathrotia.cs@gmail.com>
2018-11-10 12:50:39 -0800

    basic setup and get items list
    Signed-off-by: Pratik1005 <pratikkathrotia.cs@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+import { ItemsComponent } from './items/items.component';
+
+import { ItemService } from './item.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +21,7 @@ import { environment } from 'src/environments/environment';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/item.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, 
+         AngularFirestoreCollection, 
+         AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Item } from './item';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>
+
+  constructor(private afs: AngularFirestore) {
+    this.items = this.afs.collection('items').valueChanges();
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
