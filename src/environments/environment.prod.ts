f88fae0519384e32e1f0a6a1c1ec6b305f91ae36
Pratik1005 <pratikkathrotia.cs@gmail.com>
2018-11-10 12:16:14 -0800

    set up firebase
    Signed-off-by: Pratik1005 <pratikkathrotia.cs@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB0_9vxx5dr5a7v1hYRTx9LdMaZTUtSXho",
+    authDomain: "fir-demo-c7b21.firebaseapp.com",
+    databaseURL: "https://fir-demo-c7b21.firebaseio.com",
+    projectId: "fir-demo-c7b21",
+    storageBucket: "fir-demo-c7b21.appspot.com",
+    messagingSenderId: "710629352277"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB0_9vxx5dr5a7v1hYRTx9LdMaZTUtSXho",
+    authDomain: "fir-demo-c7b21.firebaseapp.com",
+    databaseURL: "https://fir-demo-c7b21.firebaseio.com",
+    projectId: "fir-demo-c7b21",
+    storageBucket: "fir-demo-c7b21.appspot.com",
+    messagingSenderId: "710629352277"
+  }
 };
 
 /*
